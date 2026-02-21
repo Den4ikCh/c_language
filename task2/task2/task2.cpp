732840bf5d120f8fc5a88bfa62d6097f162968f6
Den4ikCh <chuprikov.denis.vrn@gmail.com>
2026-02-21 21:46:44 +0300

    task2

---
+++ task2/task2/task2.cpp
@@ -0,0 +1,21 @@
+﻿#define _USE_MATH_DEFINES
+
+#include <math.h>
+#include <iostream>
+
+int main()
+{
+    double sum = 0;
+    double current = pow(1, 3) / 1;
+    int n = 0;
+    double epsilon = 1e-6;
+
+    while (15 * M_E - sum > epsilon) {
+        sum += current;
+        n++;
+        current *= pow(n + 1, 3) / pow(n, 4);
+    }
+
+    printf("sum = %.15f\n", sum);
+    printf("15e = %.15f\n", 15 * M_E);
+}
